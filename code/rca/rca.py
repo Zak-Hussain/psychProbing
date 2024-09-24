161d8a9c1b193e3e08a6bb90507e3bf702fe68eb
Zak Hussain <79920023+Zak-Hussain@users.noreply.github.com>
2024-09-24 11:15:38 +0200

    commit

--- code/rca/rca.py
+++ code/rca/rca.py
@@ -87,7 +87,6 @@ def checker(embed_names, y, dtype, associated_embeds, outer_cv):
         return 'associated_embed'
 
     # Check if there are too few observations
-
     test_n = int((1 / outer_cv) * len(y))
     if test_n < 20:
         return 'test_n < 20'

ad0ed1a89aa5b9af922696512ff25e561117e2e4
Zak Hussain <79920023+Zak-Hussain@users.noreply.github.com>
2024-12-02 16:00:43 +0100

    uncomment download

--- code/setup.py
+++ code/setup.py
@@ -6,35 +6,35 @@ import pickle
 
 ###### Downloading data from OSF ######
 
-# project_id = 'nrkd7'
-#
-# # Connect to OSF
-# osf = OSF()
-#
-# # Find the project
-# project = osf.project(project_id)
-#
-# # Get all files from the project
-# storage = project.storage('osfstorage')
-#
-# # Loop through all files and download them
-# for folder in storage.folders:
-#     print(f'Entering folder {folder.name}...')
-#
-#     output_dir = os.path.join('..', 'data', folder.name)
-#
-#     # Create the output directory if it doesn't exist
-#     if not os.path.exists(output_dir):
-#         os.makedirs(output_dir)
-#
-#     for file in folder.files:
-#         print(f'Downloading {file.name}...')
-#         # Create full local file path
-#         local_path = os.path.join(output_dir, file.name)
-#         # Download the file
-#         with open(local_path, 'wb') as f:
-#             file.write_to(f)
-#         print(f'Saved {file.name} to {output_dir}')
+project_id = 'nrkd7'
+
+# Connect to OSF
+osf = OSF()
+
+# Find the project
+project = osf.project(project_id)
+
+# Get all files from the project
+storage = project.storage('osfstorage')
+
+# Loop through all files and download them
+for folder in storage.folders:
+    print(f'Entering folder {folder.name}...')
+
+    output_dir = os.path.join('..', 'data', folder.name)
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for file in folder.files:
+        print(f'Downloading {file.name}...')
+        # Create full local file path
+        local_path = os.path.join(output_dir, file.name)
+        # Download the file
+        with open(local_path, 'wb') as f:
+            file.write_to(f)
+        print(f'Saved {file.name} to {output_dir}')
 
 ###### Finding brain-behavior union ######
 
